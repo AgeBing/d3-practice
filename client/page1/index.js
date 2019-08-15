da6f1ef8ad5be3ebd1ac0d55b6108e9e30cc5f23
Age <ykjage@gmail.com>
2019-08-15 22:28:19 +0800

    力引导边捆绑

--- client/page1/index.js
+++ client/page1/index.js
@@ -4,18 +4,25 @@ import $ from 'jquery';
 require("./index.scss");
 
 import * as d3Sankey from 'd3-sankey'
-import * as data from '../../data/sankey.json'
+import * as dataSankey from '../../data/sankey.json'
 import  mysankey from './sankey'
 
+import * as asd from './fdeb'
 const width = 1000
 const height = 1000
 
-let s = new mysankey(data)
-let svg = d3.select('#map-container')
-       .append('svg')
-     .attr('width',width)
-     .attr('height',height)
+function testSankey(argument) {
+	let s = new mysankey(dataSankey)
+	let svg = d3.select('#map-container')
+	       .append('svg')
+	     .attr('width',width)
+	     .attr('height',height)
 
-s.renderNodes(svg)
-s.renderLinks(svg)
-s.print()
+	s.renderNodes(svg)
+	s.renderLinks(svg)
+	s.print()
+}
+
+// testSankey()
+
+
